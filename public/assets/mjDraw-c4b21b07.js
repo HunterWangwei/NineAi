ac43ea1b43e8411a56780443c2c1010f5bff41d8
Hunter killer <43551096@qq.com>
2024-03-13 04:03:09 +0800

    3.4.1

---
+++ public/assets/mjDraw-c4b21b07.js
@@ -0,0 +1,1 @@
+import{ah as e,ai as a}from"./index-20561e6c.js";function u(r){return e({url:"/fanyi/translate",data:r})}function n(r){return a({url:"/queue/addMjDrawQueue",data:r})}function s(r){return e({url:"/midjourney/proxy",data:r,headers:{responseType:"arraybuffer"}})}export{u as a,s as b,n as f};
